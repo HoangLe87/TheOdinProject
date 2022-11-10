8f97bc85d20c4de6c9a8b7bb6f4ca572c929a76a
HoangLe87 <hongacek@hotmail.com>
2022-11-10 23:30:07 +0100

    library init

---
+++ INTERMEDIATE/library/dist/index.js
@@ -0,0 +1,26 @@
+let myLibrary = [];
+
+function Book() {
+    this.id = ''
+    this.title = ''
+}
+
+let newBook
+
+newBook.pro
+
+
+function addBookToLibrary(newBook) {
+    let id = myLibrary.length+1
+    newBook.id=id
+    myLibrary.push(newBook)
+}
+
+//a function that loops through the array and displays each book on the page
+
+
+//Add a “NEW BOOK” button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it’s been read
+
+//Add a button on each book’s display to remove the book from the library.
+
+//Add a button on each book’s display to change its read status.
