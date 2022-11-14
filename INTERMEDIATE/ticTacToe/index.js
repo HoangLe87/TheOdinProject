caeec0ffae5c07a480bc170d72d0c4fb80828ba9
HoangLe87 <hongacek@hotmail.com>
2022-11-14 21:14:00 +0100

    tic tac toe finish

--- INTERMEDIATE/ticTacToe/index.js
+++ INTERMEDIATE/ticTacToe/index.js
@@ -1,3 +1,4 @@
+// plyer object constructor
 let Player = (name, mark, gameBoard) => {
     const play = (index) => {
         if (gameBoard[index]==undefined) {
@@ -9,11 +10,23 @@ let Player = (name, mark, gameBoard) => {
     return {name, mark, play}
 }
 
+
+// inital game set up
+let initGame = (playerName) => {
+    let gameBoard=Array(9)
+    let player1 = Player(playerName, 'x', gameBoard)
+    let player2 = Player('PC', 'o', gameBoard)
+    return {gameBoard, player1, player2}
+}
+
+// wait function
 const wait = async(ms) => {
     return new Promise(resolve=>(setTimeout(resolve, ms)))
 }
 
+// game logic
 const game = (player1, player2, gameBoard) => {
+    console.log(gameBoard)
     for (let i=0; i<9; i++) {
         document.getElementById(`div${i}`).addEventListener("click", async()=> {
                 let player1Played = player1.play(i)
@@ -22,6 +35,7 @@ const game = (player1, player2, gameBoard) => {
                 if (gameOver(gameBoard)) {
                     await wait(1000)
                     endScreen(player1.name, player1.name)
+                    gameBoard=Array(9)
                 }
                 if (player1Played) {
                     while (!gameOver(gameBoard) && !tie(gameBoard)) {
@@ -33,6 +47,7 @@ const game = (player1, player2, gameBoard) => {
                             if (gameOver(gameBoard)) {
                                 await wait(1000)
                                 endScreen('PC', player1.name)
+                                gameBoard=Array(9)
                             }
                             break
                         }
@@ -40,11 +55,13 @@ const game = (player1, player2, gameBoard) => {
                 }
                 if (!gameBoard.includes(undefined) && !gameOver(gameBoard)) {
                     tieEndScreen()
+                    gameBoard=Array(9)
                 }
         })
     }
 }
 
+// win or lose logic
 const gameOver = ((gameBoard) => {
     if ( 
         (gameBoard[0]!=undefined && gameBoard[0] === gameBoard[1] && gameBoard[2] === gameBoard[0] && gameBoard[1] === gameBoard[2]) ||
@@ -60,12 +77,14 @@ const gameOver = ((gameBoard) => {
         }
 })
 
+// tie logic
 const tie = (gameBoard) => {
     if ((gameBoard.length === 9) && (!gameBoard.includes(undefined))) {
         return true
     }
 }
 
+// display the array
 const display = (gameBoard) => {
     for (let i=0;i<gameBoard.length; i++) {
         gameBoard[i]?document.getElementById(i).textContent=gameBoard[i]:document.getElementById(i).textContent=""
@@ -73,15 +92,14 @@ const display = (gameBoard) => {
 
 }
 
+// the start 
 const ticTacToe = ((playerName) => {
-    let gameBoard = Array(9)
+    let {player1, player2, gameBoard} = initGame(playerName)
     display(gameBoard)
-    let player1 = Player(playerName, 'x', gameBoard)
-    let player2 = Player('player2', 'o', gameBoard)
     game(player1, player2, gameBoard)
 })
 
-
+// start button function
 const startGame = async() => {
     let playerName = document.getElementById("name").value
     let div = document.createElement("div")
@@ -95,10 +113,12 @@ const startGame = async() => {
     ticTacToe(playerName)
 }
 
+// wait onload for start button
 window.addEventListener("load", () => {
     document.getElementById("startButton").addEventListener("click", ()=> startGame())
 })
 
+// end screen win or lose
 const endScreen = (winner, player1) => {
     let message = document.createElement("div")
     if (winner === player1) {
@@ -129,21 +149,33 @@ const endScreen = (winner, player1) => {
     message.classList.toggle('congratulate')
 }
 
-const tieEndScreen = (playerName) => {
-    let message = document.getElementById("start")
+
+// end screen for tie
+const tieEndScreen = () => {
+    let message = document.createElement("div")
     message.textContent=`It is a draw!`
-    message.classList.toggle('congratulate')
     let main = document.getElementById("main")
     main.classList.toggle('hidden')
+    let start = document.getElementById("start")
+    start.classList.toggle('hidden')
     let div = document.createElement("div")
     let playAgainButton = document.createElement("button")
-    playAgainButton.setAttribute('id', 'playAgainButton')
     playAgainButton.textContent="play again"
-    playAgainButton.addEventListener('click', reset)
+    playAgainButton.addEventListener('click', ()=>{
+        message.textContent=''
+        message.classList.remove('congratulate')
+        start.classList.toggle('hidden')
+        main.classList.toggle('hidden')
+        reset()
+    })
     div.appendChild(playAgainButton)
     message.appendChild(div)
+    let body = document.getElementsByTagName("BODY")[0];
+    body.appendChild(message)
+    message.classList.toggle('congratulate')
 }
 
+// play gain button
 const reset = () => {
     let playerName = document.getElementById("playerName").textContent
     ticTacToe(playerName)
